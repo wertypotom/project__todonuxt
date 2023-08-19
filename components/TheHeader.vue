a55f63dbbd2f3d7e9e360686b3e570fc01b6b362
Andrey Povstyanko <werty.potom@gmail.com>
2023-08-19 13:48:07 +0600

    created starter files

---
+++ components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="navbar">
+    <h2 @click="$router.push('/')">Vue 3</h2>
+    <div class="navbar__btns">
+      <my-button @click="$router.push('/posts')">Posts</my-button>
+      <my-button @click="$router.push('/about')">About</my-button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  cursor: pointer;
+  color: teal;
+}
+.navbar {
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: paleturquoise;
+}
+.navbar__btns {
+  display: flex;
+  gap: 10px;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This is a description page about the App</h1>
+</template>
